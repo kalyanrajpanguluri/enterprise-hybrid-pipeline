98794c427f7499d2489ad907719a3d4731881b25
kalyanrajpanguluri <kalyanrajpanguluri@gmail.com>
2026-08-08 23:18:08 +0530

    style: fix tag equal-sign alignment in provider.tf

--- provider.tf
+++ provider.tf
@@ -22,9 +22,9 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "Terraform"
+      Project      = var.project_name
+      Environment  = var.environment
+      ManagedBy    = "Terraform"
       Architecture = "Medallion-S3-Data-Lake"
     }
   }
